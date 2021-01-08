54970f88c67b3bc89cc973dd2151dbcdd68671f7
ByVerduXx <danielverdurasgallego@gmail.com>
2021-01-08 14:19:33 +0100

    Fase 6 comentada y terminada

--- archivos_c_h_apoyo/ejecutar.c
+++ archivos_c_h_apoyo/ejecutar.c
@@ -14,6 +14,8 @@ pid_t ejecutar_orden(const char *orden, int *pbackgr)
    char **args;
    pid_t pid;
    int indice_ent = -1, indice_sal = -1,entrada = 0,salida = 1; /* por defecto, no hay < ni > */
+   int entrada_estandar = dup(0),salida_estandar = dup(1);  //guardamos los descriptores de entrada y salida estandar
+   char * nombre;       //copiamos el nombre del archivo de salida en caso de que exista
    
    if ((args = parser_orden(orden, &indice_ent, &indice_sal, pbackgr)) == NULL)
    {
@@ -21,44 +23,44 @@ pid_t ejecutar_orden(const char *orden, int *pbackgr)
    }
    if(indice_ent!=-1)
    {
-      redirec_entrada(args,indice_ent,&entrada);
+      redirec_entrada(args,indice_ent,&entrada);  //si hay una redireccion, llama a la funcion
       
    }
    if(indice_sal!=-1){
-
-      redirec_salida(args,indice_sal,&salida);
-
+      nombre = args[indice_sal + 1];   //le asignamos el nombre del archivo de salida
+      redirec_salida(args,indice_sal,&salida);  //si hay una redireccion, llama a la funcion
    }
-   
-
       pid = fork(); //Creamos la minishell hija
 
       if (pid == 0){ //Si es la minishell hija
 
          if(entrada!= 0){
 
-            dup2(entrada, STDIN_FILENO);
+            dup2(entrada, STDIN_FILENO);   //cambia la entrada estandar por el archivo
          }
 
          if(salida!=1)
          {
-            dup2(salida,STDOUT_FILENO);
+            dup2(salida,STDOUT_FILENO);   //cambia la salida estandar por el archivo
          }
 
          execvp(args[0],args);//Ejecuta la orden
-         dup2(0,STDIN_FILENO);   //si falla porque no existe la orden vuelve a poner los descriptores de entrada y salida como estaban para 
-         dup2(1,STDOUT_FILENO);  //escribir el error por pantalla
+         dup2(salida_estandar,STDOUT_FILENO);  //si falla porque no existe la orden vuelve a poner los descriptores de entrada y salida como estaban para 
+         dup2(entrada_estandar,STDIN_FILENO);  //escribir el error por pantalla
          printf("Esa orden no existe.\n");
+         if(indice_sal){   //si habia redireccion de salida
+            remove(nombre); //borra el archivo creado
+         }
          exit(-1);
 
       }
       else{ 
          free_argumentos(args); //Libera la memoria dinamica
          if(salida != 1){
-            dup2(1,STDOUT_FILENO);
+            dup2(salida_estandar, STDOUT_FILENO);  //vuelve a poner la salida estandar
          }
          if(entrada != 0){
-            dup2(0,STDIN_FILENO);
+            dup2(entrada_estandar,STDIN_FILENO);   //vuelve a poner la entrada estandar
          }
          return pid; //Devuelve el pid del hijo
       }
